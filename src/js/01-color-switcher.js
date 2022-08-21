6644d973a6b36bc0e11abe88e1cf04ebec73a28d
Nick Zheliba <nikolaj.jeliba@gmail.com>
2022-08-21 22:08:03 +0300

    add JS for task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+     body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', 'true');
+    stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+   startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'true');
+});
